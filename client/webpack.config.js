6b28ab287aa4927ca6480126e1432208d4b24259
Money Maker <mitchell.harris@westnet.com.au>
2023-01-08 18:30:30 +0800

    added sist folder and final updates

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = () => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: '.index.html',
+        template: './index.html',
         title: 'JATE'
       }),
       new InjectManifest({
